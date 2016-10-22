df656dfeb28f14b57067daf6643c568427cd5e5b
Raj <rajeev.v.singh@gmail.com>
2016-10-22 11:00:37 -0400

    Implement basic todo functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,57 @@
 import { Component } from '@angular/core';
 
+class Todo {
+  id: number;
+  description: string;
+  done: boolean;
+  constructor(id: number, description: string) {
+    this.id = id;
+    this.description = description;
+    this.done = false;
+  }
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `
+      <section id="todo-app">
+        <h1>Angular2 Simple Todo</h1>
+        <section id="todos">
+          <h2>Todos</h2>
+          <ul>
+            <li *ngFor="let todo of todos">
+              <input type="checkbox" [(ngModel)]="todo.done">
+              <span class="todo-description">{{todo.description}}</span>
+              <button type="button" (click)="deleteTodo(todo.id)">X</button>
+            </li>
+          </ul>
+          <form (ngSubmit)="createTodo(todoForm, todo)" #todoForm="ngForm">
+            <input type="text" (ngModel)="newTodoInput" class="form-control" 
+              name="todoDescription" required placeholder="Add a new task">
+            <button type="submit" class="btn btn-default">Add todo</button>
+          </form>
+        </section>
+      </section>
+      `
 })
-export class AppComponent { }
+export class AppComponent {
+  id = 0;
+  title = 'Generic Todo';
+  todos = [ 
+    new Todo(this.id++, 'Go grocery shopping'), 
+    new Todo(this.id++, 'Walk dog'), 
+  ];
+  createTodo = function(todoForm) {
+    let todo = new Todo(this.id++, todoForm.value.todoDescription);
+    this.todos.push( todo );
+    todoForm.reset();
+
+    event.preventDefault();
+  }
+  deleteTodo = function(id) {
+    this.todos = this.todos.filter( (todo) => todo.id !== id);
+
+    event.preventDefault();
+  }
+
+}
